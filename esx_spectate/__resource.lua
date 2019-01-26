59dc1e70455f8db9b2131c71047f25fb7b547cc2
niiK0 <37949374+niiK0@users.noreply.github.com>
2019-01-26 03:40:49 +0000

    Add files via upload

---
+++ esx_spectate/__resource.lua
@@ -0,0 +1,16 @@
+##EDITED BY niiK0 - https://github.com/niiK0/niiK0-Spectate
+
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+server_script 'server/main.lua'
+client_script 'client/main.lua'
+
+ui_page {
+  'ui/index.html'
+}
+
+files {
+  'ui/index.html',
+  'ui/style.css',
+  'ui/main.js',
+}
